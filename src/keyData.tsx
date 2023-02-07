e8b88e38737801fa5b41247799b3ebc7f4fcc3c3
Yu-Jia Huang <shiusun246@gmail.com>
2023-02-08 01:15:38 +0800

    [modify] gitignore

---
+++ src/keyData.tsx
@@ -0,0 +1,4 @@
+require('dotenv').config()
+export const weatherAuth = process.env.WEATHER_AUTH
+export const youtubeAuth = process.env.YOUTUBE_AUTH
+export const googleMapAuth = process.env.GOOGLEMAP_AUTH
