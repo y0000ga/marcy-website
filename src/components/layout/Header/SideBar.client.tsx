fec5550eaf3f168340ef73b28a518e2ac7a3f9dc
Yu-Jia Huang <shiusun246@gmail.com>
2024-05-21 22:46:01 +0800

    fix: close sidebar when routing

--- src/components/layout/Header/SideBar.client.tsx
+++ src/components/layout/Header/SideBar.client.tsx
@@ -1,11 +1,18 @@
 'use client'
 
 import { IWrapper } from '@/type/common.type'
-import { useState } from 'react'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
 import { RxHamburgerMenu } from 'react-icons/rx'
 
 export const Sidebar = ({ children }: IWrapper) => {
   const [isToggle, setIsToggle] = useState(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setIsToggle(false)
+  }, [pathname])
+
   return (
     <>
       <RxHamburgerMenu
